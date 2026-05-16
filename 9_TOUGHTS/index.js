7781c1b7bdbf599cf26f50980e7e0cdcfa726ffd
maarcos-wesleey <maarcoswesleey@gmail.com>
2026-05-15 21:27:16 -0300

    Atualização 15/05 2

--- 9_TOUGHTS/index.js
+++ 9_TOUGHTS/index.js
@@ -13,6 +13,13 @@ const conn = require('./db/conn')
 const Tought = require('./models/Tought')
 const User = require('./models/User')
 
+//Import Routes
+
+const toughtsRoutes = require('./routes/toughtsRoutes')
+
+//Import Controller
+const ToughtController = require('./controllers/ToughtController')
+
 
 app.engine('handlebars', exphbs.engine({}))
 app.set('view engine', 'handlebars')
@@ -62,6 +69,12 @@ app.use((req, res, next) => {
 
     next()
 })  
+
+
+//Routes
+app.use('/toughts', toughtsRoutes)
+
+app.get('/', ToughtController.showToughts)
 
 conn
     // .sync({force: true})
--- 9_TOUGHTS/routes/toughtsRoutes.js
+++ 9_TOUGHTS/routes/toughtsRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router()
-//controller
+const ToughtController = require('../controllers/ToughtController')
 
-router.get('/', ToughtsController.showToughts)
+router.get('/', ToughtController.showToughts)
 
 module.exports = router
